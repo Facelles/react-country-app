be0f741d57e826a922b60df95208729897509bc3
Facelles <122049357+Facelles@users.noreply.github.com>
2025-07-25 15:26:11 +0300

    add modal

--- src/components/ModalPreview.tsx
+++ src/components/ModalPreview.tsx
@@ -7,10 +7,19 @@ interface ModalPreviewProps {
 
 const ModalPreview = ({ country, onClose }: ModalPreviewProps) => {
   return (
-    <div>
-      <button onClick={onClose}>Close</button>
-      <h2>{country.name}</h2>
-      <img src={country.flags.png} alt={`${country.name} flag`} />
+    <div className='absolute top-16 left-0 w-full max-w-none h-[calc(100vh-1rem)] bg-white z-50 p-8 overflow-y-auto'>
+      <button onClick={onClose} className='mb-8 bg-white shadow px-6 py-2 rounded font-medium absolute left-4'> ← Back </button>
+
+
+
+      <div className="grid grid-cols-2 gap-16 items-start absolute top-22">
+        <div>
+          <img src={country.flags.png}
+          alt={country.name}
+          className="w-full max-w-[200px] object-contain rounded shadow" />
+       </div>
+      </div>
+
     </div>
   );
 };
